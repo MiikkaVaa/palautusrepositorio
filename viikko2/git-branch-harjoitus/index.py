f8476871b9eb68b4c12f496fc4d5322634fc2f09
MiikkaVaa <miikka.vaananen@helsinki.fi>
2025-11-08 19:36:28 +0200

    kloonin branchissa tulo tehty uusi tiedosto ja muokattu index.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -3,6 +3,7 @@
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo
 
 logger("aloitetaan ohjelma ") # muutos mainissa
 
@@ -11,6 +12,7 @@ y = int(input("luku 2: "))
 
 print(f"{x} + {y} = {summa(x ,y)}") # muutos mainilla
 print(f"{x} - {y} = {erotus(x ,y)}") # muutos mainilla
+print(f"{x} * {y} = {tulo(x, y)}") # tehty koonin tulossa
 
 logger("lopetetaan")
 print("goodbye") # lisäys bugikorjauksen-branchissa
